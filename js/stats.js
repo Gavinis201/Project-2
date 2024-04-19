48508eed67182afe8ec829fc69d0b0c0b8109c93
itloaf <jcarterk12k@gmail.com>
2024-04-18 21:21:24 -0600

    commented js code, etc

--- js/stats.js
+++ js/stats.js
@@ -4,6 +4,8 @@ fetch("https://alnyb0ty3i.execute-api.us-east-1.amazonaws.com/sportsData")
 
     function doTheThingHarold(inputData) {
         
+
+        //This function adds an item and divider in the dropdown menu for each team in the list.
         for (let team in inputData) {
             const item = document.createElement("li");
             item.classList.add("dropdown-item");
@@ -22,12 +24,14 @@ fetch("https://alnyb0ty3i.execute-api.us-east-1.amazonaws.com/sportsData")
         }
     }
     
+
     // Set a default value for the table as a "reset point" so the old data clears when you select a new team
     const tableData = document.getElementById("content-table").outerHTML;
     
-    // Function: you select a team and it will display their roster/stats
+    // Function: you select a team and it will display their roster/stats/games
     function filterData(selectedTeam) {
         
+        //team stats
         document.getElementById("team-name").innerHTML = selectedTeam["name"];
         document.getElementById("record-number").innerHTML = selectedTeam["current_record"];
         document.getElementById("points-number").innerHTML = ":  " + Math.floor(selectedTeam.statistics.avgPoints.value);
@@ -44,8 +48,7 @@ fetch("https://alnyb0ty3i.execute-api.us-east-1.amazonaws.com/sportsData")
         document.getElementById("img2").src = selectedTeam.logo_light;
 
 
-
-
+    //creates a table and populates it with the roster data.
     document.getElementById("content-table").innerHTML = tableData;
     for (player in selectedTeam.roster) {
         const thing = document.createElement("tr");
@@ -57,13 +60,12 @@ fetch("https://alnyb0ty3i.execute-api.us-east-1.amazonaws.com/sportsData")
         document.getElementById("content-table").appendChild(thing);
     }
 
+    //edits the existing element to contain data from the last game.
+    //Is currently incomplete, but changing image sources and appending multiple items via a loop are demonstrated in the rosterstats table.
     for (game in selectedTeam.last_five_games) {
-        console.log(selectedTeam.last_five_games[game]);
         document.getElementById("hometeam1").innerHTML = selectedTeam.last_five_games[game].home_team.team_name_abbreviation;
         document.getElementById("awayteam1").innerHTML = selectedTeam.last_five_games[game].away_team.team_name_abbreviation;
-        let hometeam = selectedTeam.last_five_games[game].home_team.team_name;
-        let awayteam = selectedTeam.last_five_games[game].away_team.team_name;
-        document.getElementById("homeimg1").src = [hometeam].name;
-        console.log(document.getElementById("homeimg1").src = [hometeam].name);
+        document.getElementById("homescore1").innerHTML = selectedTeam.last_five_games[game].home_team.score;
+        document.getElementById("awayscore1").innerHTML = selectedTeam.last_five_games[game].away_team.score;
     }
 }
